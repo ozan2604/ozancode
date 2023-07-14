a18059b35c83da5c9ca6ed37e42c1253a76d8c00
ozan2604 <123476414+ozan2604@users.noreply.github.com>
2023-07-14 12:40:32 +0300

    Create HelloWorld.vue
    ilk proje

---
+++ HelloWorld.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <div class="box" :style="{backgroundColor : color}"></div>
+    <div class="box2" :style="customStyle"></div>
+    <div class="box3" :style="[customStyle , {witdh: yukseklik + 'px'}]"></div>
+    
+    <hr>
+    <input type="text" v-model="color">
+    <input type="text" v-model="height">
+    
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      color:"green",
+      height: 100
+      }
+  
+  },
+  computed : {
+        customStyle: function(){
+          return{
+            backgroundColor: this.color,
+            height : this.height + "px"
+          }
+        }
+
+      }
+}
+</script>
+<style >
+.box{
+  width: 100px;
+  height: 100px;
+  background-color: gray;
+  display: inline-block;
+  margin-right: 5px;
+}
+
+.box2{
+  width: 150px;
+  height: 150px;
+  background-color: gray;
+  display: inline-block;
+  margin-right: 5px;
+}
+.box3{
+  width: 200px;
+  height: 200px;
+  background-color:gray;
+  display: inline-block;
+  margin-right: 5px;
+}
+.greeen{
+  background-color: green;
+}
+.blue{
+  background-color: blue;
+}
+.red{
+  background-color: red;
+}
+
+
+</style>
